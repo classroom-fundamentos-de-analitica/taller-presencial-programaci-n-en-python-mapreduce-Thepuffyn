89413d50e9ae185e580f155671f5cfbf1b5eaaa0
Juan Jose Marin Alvarez <102542272+Thepuffyn@users.noreply.github.com>
2024-02-20 21:05:23 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -14,133 +14,70 @@
 #   ]
 #
 
+"""Taller evaluable"""
+
 import glob
-import fileinput
+
+import pandas as pd
+
 
 def load_input(input_directory):
+    """Load text files in 'input_directory/'"""
+    #
+    # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
+    # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
+    # entrada en el DataFrame.
+    #
 
-    filenames = glob.glob(input_directory + "/*.*")
+    filenames = glob.glob(input_directory + '/*.*')
+    dataframes = [
+        pd.read_csv(filename, index_col = None, sep=';', names=['text']) for filename in filenames
+    ]
+    dataframe = pd.concat(dataframes).reset_index(drop=True)
+    return dataframe
+def clean_text(dataframe):
+    """Text cleaning"""
+    #
+    # Elimine la puntuación y convierta el texto a minúsculas.
+    #
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.replace(',', '').str.replace('.','').str.lower()
+    return dataframe
 
-    sequence = []
+def count_words(dataframe):
+    """Word count"""
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.split(' ')
+    dataframe = dataframe.explode('text').reset_index(drop=True) #Voltea la lista de palabras en filas de dataframe
+    dataframe = dataframe.rename(columns = {'text':'word'})
+    dataframe['count'] = 1
 
-    with fileinput.input(files = filenames) as f:
-        for line in f:
-            sequence.append(
-                (f.filename(), line)
-                )
-
-    return sequence
-
-#
-# Escriba una función llamada maper que recibe una lista de tuplas de la
-# función anterior y retorna una lista de tuplas (clave, valor). En este caso,
-# la clave es cada palabra y el valor es 1, puesto que se está realizando un
-# conteo.
-#
-#   [
-#     ('Analytics', 1),
-#     ('is', 1),
-#     ...
-#   ]
-#
-
-def mapper(sequence):
-
-    new_sequence = [
-        (word.lower().replace(".","").replace(",",""), 1)
-        for _, line in sequence
-        for word in line.split()
-        ]
-    return new_sequence
-
-#
-# Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
-# por el mapper, y retorna una lista con el mismo contenido ordenado por la
-# clave.
-#
-#   [
-#     ('Analytics', 1),
-#     ('Analytics', 1),
-#     ...
-#   ]
-#
-
-def shuffle_and_sort(sequence):
-    sequence = sorted(
-        sequence,
-         key = lambda x: x[0]
+    conteo = dataframe.groupby(['word'], as_index = False).agg(
+        {
+            'count': sum
+        }
     )
-    return sequence
+    return conteo
 
-#
-# Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
-# reduce los valores asociados a cada clave sumandolos. Como resultado, por
-# ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
-# texto.
-#
+def save_output(dataframe, output_filename):
+    """Save output to a file."""
+    dataframe.to_csv(output_filename, sep=';', index=False)
 
-from itertools import groupby
-
-def reducer(sequence):
-    new_sequence = []
-    for k, g in groupby(sequence, lambda x: x[0]):
-        key = k
-        value = sum([x[1] for x in g])
-        new_sequence.append(
-            (key, value)
-        )
-    return new_sequence
-
-#
-# Escriba la función create_ouptput_directory que recibe un nombre de directorio
-# y lo crea. Si el directorio existe, la función falla.
-#
-
-import os.path
-
-def create_ouptput_directory(output_directory):
-
-    if os.path.isdir(output_directory):
-        raise Exception("El directorio ya existe")
-    os.mkdir(output_directory)
-
-#
-# Escriba la función save_output, la cual almacena en un archivo de texto llamado
-# part-00000 el resultado del reducer. El archivo debe ser guardado en el
-# directorio entregado como parámetro, y que se creo en el paso anterior.
-# Adicionalmente, el archivo debe contener una tupla por línea, donde el primer
-# elemento es la clave y el segundo el valor. Los elementos de la tupla están
-# separados por un tabulador.
-#
-def save_output(output_directory, sequence):
-    filename = os.path.join(output_directory, "part 00000")
-    with open(filename, "w") as f:
-        for key, value in sequence:
-            f.write(f"{key}\t{value}\n")
-
-#
-# La siguiente función crea un archivo llamado _SUCCESS en el directorio
-# entregado como parámetro.
-#
-def create_marker(output_directory):
-    with open(os.path.join(output_directory, "_SUCCESS"), "w") as f:
-        f.write("")
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
-def job(input_directory, output_directory):
-    sequence = load_input("input/")
-    sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
-    sequence = reducer(sequence)
-    create_ouptput_directory("output/")
-    save_output("output/", sequence)
-    create_marker("output")
+def run(input_directory, output_filename):
+    """Call all functions."""
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    save_output(dataframe, output_filename)
+    
 
 
 if __name__ == "__main__":
-    job(
+    run(
         "input",
-        "output",
+        "output.txt",
     )
